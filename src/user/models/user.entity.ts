14726059f0d4600e05197d6609e0107d59ab7dbf
awahids <awahid.safhadi@gmail.com>
2022-01-11 02:19:13 +0800

    feature user but not fix

---
+++ src/user/models/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UserRole } from './user.interface';
+
+@Entity()
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: false })
+  passwrod: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+}
