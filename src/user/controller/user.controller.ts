301e4f0f7c6f43fb32c91a839a96fbf5f07b219e
awahids <awahid.safhadi@gmail.com>
2022-01-12 01:29:35 +0800

    expand user controller

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,7 +1,23 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  InternalServerErrorException,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from 'src/auth/service/auth.service';
 import { UserService } from '../service/user.service';
 import * as bcrypt from 'bcrypt';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-guard';
+import { UserIsUserGuard } from 'src/auth/guards/UserIsUser.guard';
+import { User, UserRole } from '../models/user.interface';
+import { hasRoles } from 'src/auth/decorators/role.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('user')
 export class UserController {
@@ -82,6 +98,79 @@ export class UserController {
       const token = await this.authService.generateJWT(findUser);
 
       return { token };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, UserIsUserGuard)
+  async findOne(@Param('id') id: string): Promise<User> {
+    try {
+      const findUser = await this.userService.findOne({
+        where: { id },
+        relations: ['registeredSeminar', 'registeredSeminar.seminar'],
+      });
+
+      delete findUser.password;
+      delete findUser.role;
+
+      return findUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @hasRoles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get()
+  async findAll(): Promise<User[]> {
+    try {
+      const users = await this.userService.findAll();
+
+      users.forEach(function (v) {
+        delete v.password;
+        delete v.role;
+      });
+
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, UserIsUserGuard)
+  async deleteOne(@Param('id') id: string): Promise<string> {
+    try {
+      const deleteUser = await this.userService.deleteOne(Number(id));
+
+      if (!deleteUser) {
+        throw new InternalServerErrorException('Unable to delete data');
+      }
+
+      return 'Delete Successfully';
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard, UserIsUserGuard)
+  async updateOne(@Param('id') id: string, @Body() user: User): Promise<User> {
+    try {
+      const updateData = await this.userService.updateOne(Number(id), user);
+
+      if (!updateData) {
+        throw new InternalServerErrorException('Unable to update data');
+      }
+
+      const getData = await this.userService.findOne(Number(id));
+
+      delete getData.password;
+      delete getData.role;
+
+      return getData;
     } catch (error) {
       throw error;
     }
